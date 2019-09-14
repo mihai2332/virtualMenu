94485261d81fe18e9b21a3f24c681d964c514caf
mihai ciurca <Mihai.Ciurca@orange.com>
2019-09-14 18:49:28 +0300

    work in progress, tested

--- src/main/java/com/spliff/virtualmenu/controller/CategoryController.java
+++ src/main/java/com/spliff/virtualmenu/controller/CategoryController.java
@@ -1,8 +1,6 @@
 package com.spliff.virtualmenu.controller;
 
-import com.spliff.virtualmenu.entity.Category;
 import com.spliff.virtualmenu.entity.Product;
-import com.spliff.virtualmenu.service.CategoryService;
 import com.spliff.virtualmenu.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -14,23 +12,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.Set;
 
 @Controller
-@RequestMapping(value = "/restaurant")
+@RequestMapping(value = "/category")
 public class CategoryController {
 
     @Autowired
-    CategoryService categoryService;
-    @Autowired
     ProductService productService;
 
-    @GetMapping(value = "/{uuid}/category")
-    public ResponseEntity getAllCategories(@PathVariable(name = "uuid") String restaurantUUID) {
-        Set<Category> categories = categoryService.getAllCategories(restaurantUUID);
-        return ResponseEntity.ok(categories);
-    }
-
-    @GetMapping(value = "/{uuid}/category/{id}")
-    public ResponseEntity getAllCategories(@PathVariable(name = "uuid") String restaurantUUID,
-                                           @PathVariable(name = "id") Integer categoryId) {
+    @GetMapping(value = "/{id}")
+    public ResponseEntity getAllCategories(@PathVariable(name = "id") Integer categoryId) {
         Set<Product> products = productService.getAllProductsFromCategory(categoryId);
         return ResponseEntity.ok(products);
     }
