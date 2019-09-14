9dba3703f1237bbfd94284a614d46a762a8fd52a
mihai ciurca <Mihai.Ciurca@orange.com>
2019-09-14 11:18:59 +0300

    initial commit

---
+++ src/test/java/com/spliff/virtualmenu/VirtualmenuApplicationTests.java
@@ -0,0 +1,16 @@
+package com.spliff.virtualmenu;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VirtualmenuApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
