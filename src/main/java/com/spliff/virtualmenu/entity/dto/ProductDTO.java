1da4b0fea6dfda1b7f411157fee5f72dc208123b
HappyNightQQ <blajalexandru16@gmail.com>
2019-11-05 00:44:20 +0200

    antmatcher on product/picture editProduct

--- src/main/java/com/spliff/virtualmenu/entity/dto/ProductDTO.java
+++ src/main/java/com/spliff/virtualmenu/entity/dto/ProductDTO.java
@@ -1,8 +1,12 @@
 package com.spliff.virtualmenu.entity.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ProductDTO {
     public String name;
     public String description;
     public Integer price;
     public Integer category;
+    public Integer id;
 }
