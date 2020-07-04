98cf13feb8087721ee401f057d01622750120334
HappyNightQQ <blajalexandru16@gmail.com>
2020-07-04 15:00:07 +0300

    switched aws to frankfurt

--- src/main/java/com/spliff/Virtualmenu/config/WebSecurityConfig.java
+++ src/main/java/com/spliff/Virtualmenu/config/WebSecurityConfig.java
@@ -54,8 +54,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable().
-                authorizeRequests()
+        http.cors().and().csrf().disable()
+                .authorizeRequests()
                 .antMatchers("/api/auth/**",
                         "/restaurant/**",
                         "/order/**",
